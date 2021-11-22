93354f3ec9c8937dc0b1af43239b6a0efcad433f
Martin Pham <tuyentq2009@gmail.com>
2021-11-22 12:36:51 +0100

    no message

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -5,21 +5,17 @@ const fs = require('fs')
 
 module.exports = async function (deployer, network, accounts) {
   if(!process.env.DEPLOYED_ADDRESS) {
-    try {
-      console.log('Deploying')
-      const contract = await deployProxy(HelloWorld, [accounts[0]], {
-        deployer,
-        initializer: 'initialize'
-      })
-  
-      console.log('Deployed', contract.address)
-  
-      fs.writeFileSync(__dirname + '/../build/README.md', `
-  **Deployed Address:** [${contract.address}](https://contract.mph.am/?json=https://raw.githubusercontent.com/${process.env.GITHUB_REPOSITORY}/${process.env.DEPLOY_BRANCH}/build/contracts/HelloWorld.json)
-      `)
-      fs.writeFileSync(__dirname + '/../build/DEPLOYED_ADDRESS.txt', contract.address)
-    } catch (err) {
-      console.log('Deploy failed', err)
-    }
+    console.log('Deploying')
+    const contract = await deployProxy(HelloWorld, [accounts[0]], {
+      deployer,
+      initializer: 'initialize'
+    })
+
+    console.log('Deployed', contract.address)
+
+    fs.writeFileSync(__dirname + '/../build/README.md', `
+**Deployed Address:** [${contract.address}](https://contract.mph.am/?json=https://raw.githubusercontent.com/${process.env.GITHUB_REPOSITORY}/${process.env.DEPLOY_BRANCH}/build/contracts/HelloWorld.json)
+    `)
+    fs.writeFileSync(__dirname + '/../build/DEPLOYED_ADDRESS.txt', contract.address)
   }
 }
